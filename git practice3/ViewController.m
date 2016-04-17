d6ba59a32739fe4a847eb0fb5d134833a5bb4eea
SDKJtechnology <386182389@qq.com>
2016-04-17 17:28:54 +0800

    第二次提交

--- git practice3/ViewController.m
+++ git practice3/ViewController.m
@@ -20,6 +20,11 @@
     UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(100, 100, 50, 50)];
     btn.backgroundColor = [UIColor redColor];
     [self.view addSubview:btn];
+    
+    UIButton *btn2 = [[UIButton alloc]initWithFrame:CGRectMake(20, 40, 200, 200)];
+    btn2.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:btn2];
+    
 }
 
 
